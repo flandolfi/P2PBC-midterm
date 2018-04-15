d98a6709396bbd476f7b6eff8c0c30d98422a39c
Francesco Landolfi <flandolfi@users.noreply.github.com>
2018-04-15 15:42:12 +0200

    Add files via upload

---
+++ src/Identifier.java
@@ -0,0 +1,76 @@
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Identifier implements Comparable<Identifier> {
+    private static int bits = 8;
+    private static BigInteger base = BigInteger.ONE.shiftLeft(bits);
+    private static MessageDigest digest;
+    private final BigInteger id;
+
+    static {
+        try {
+            digest = MessageDigest.getInstance("SHA");
+        } catch (NoSuchAlgorithmException ignore) {}
+    }
+
+    public static void setBitLength(int length) {
+        bits = length;
+        base = BigInteger.ONE.shiftLeft(bits);
+    }
+
+    public static int getBitLength() {
+        return bits;
+    }
+
+    public Identifier(BigInteger bigInteger) {
+        id = bigInteger.mod(base);
+    }
+
+    public Identifier(byte[] bytes) {
+        id = new BigInteger(1, digest.digest(bytes)).shiftRight(8*digest.getDigestLength() - bits);
+    }
+
+    public Identifier add(BigInteger bigInteger) {
+        return new Identifier(id.add(bigInteger));
+    }
+
+    public Identifier subtract(BigInteger bigInteger) {
+        return new Identifier(id.subtract(bigInteger));
+    }
+
+    public boolean isBetween(Identifier left, Identifier right) {
+        if (left.id.equals(right.id))
+            return false;
+
+        if (left.id.compareTo(right.id) < 0) {
+            return left.id.compareTo(id) < 0 && id.compareTo(right.id) <= 0;
+        }
+
+        return isBetween(left, new Identifier(base.subtract(BigInteger.ONE)))
+                || isBetween(new Identifier(BigInteger.ZERO), right);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Identifier)
+            return id.equals(((Identifier) obj).id);
+
+        return false;
+    }
+
+    @Override
+    public int compareTo(Identifier identifier) {
+        return id.compareTo(identifier.id);
+    }
+
+    @Override
+    public String toString() {
+        return id.toString();
+    }
+}
---
+++ src/Node.java
@@ -0,0 +1,96 @@
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.util.*;
+import java.util.Map.Entry;
+
+public class Node {
+    private Identifier id;
+    private InetAddress address;
+    private Node[] fingerTable;
+    private Node predecessor;
+
+    public Node(InetAddress address) {
+        this.address = address;
+        this.id = new Identifier(address.getAddress());
+        this.fingerTable = new Node[Identifier.getBitLength()];
+    }
+
+    public InetAddress getAddress() {
+        return address;
+    }
+
+    public Identifier getId() {
+        return id;
+    }
+
+    public void updateFingerTable(TreeMap<Identifier, Node> network) {
+        BigInteger gap = BigInteger.ONE;
+        Entry<Identifier, Node> entry = network.lowerEntry(id);
+        predecessor = entry == null? network.lastEntry().getValue() : entry.getValue();
+
+        for (int i = 0; i < Identifier.getBitLength(); i++) {
+            entry = network.ceilingEntry(id.add(gap));
+            fingerTable[i] = entry == null? network.firstEntry().getValue() : entry.getValue();
+            gap = gap.shiftLeft(1);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return id.toString();
+    }
+
+    public String toSIF() {
+        StringBuilder result = new StringBuilder();
+        result.append(id.toString()).append(" link");
+
+        for (Node link: fingerTable) {
+            result.append(" ").append(link.toString());
+        }
+
+        return result.append("\n").toString();
+    }
+
+    public String toDOT() {
+        StringBuilder result = new StringBuilder();
+        result.append(id.toString()).append(" [label=\"").append(address.toString()).append("\"]; ")
+                .append(id.toString()).append(" -> {");
+
+        for (Node link: fingerTable) {
+            result.append(" ").append(link.toString());
+        }
+
+        return result.append(" }\n").toString();
+    }
+
+    public List<Node> getPathTo(Identifier identifier) {
+        ArrayList<Node> result = new ArrayList<>();
+        result.add(this);
+
+        if (identifier.isBetween(predecessor.id, id))
+            return result;
+
+        if (identifier.isBetween(id, fingerTable[0].id)) {
+            result.add(fingerTable[0]);
+
+            return result;
+        }
+
+        Node predecessor = findPredecessor(identifier);
+
+        if (predecessor == this)
+            return result;
+
+        result.addAll(predecessor.getPathTo(identifier));
+
+        return result;
+    }
+
+    private Node findPredecessor(Identifier identifier) {
+        for (int i = Identifier.getBitLength() - 1; i >= 0 ; i--)
+            if (!fingerTable[i].id.equals(identifier) && fingerTable[i].id.isBetween(id, identifier))
+                return fingerTable[i];
+
+        return this;
+    }
+}
