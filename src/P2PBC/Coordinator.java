d820b3666af23867a5e85fc17e8150e95ef91319
Francesco Landolfi <fran.landolfi@gmail.com>
2018-04-18 14:13:12 +0200

    Switch to histograms

--- src/P2PBC/Coordinator.java
+++ src/P2PBC/Coordinator.java
@@ -78,7 +78,7 @@ public class Coordinator {
     }
 
     public static void main(String[] args) {
-        Integer nBits = 16, nNodes = 1024, nIters = 1024;
+        Integer nBits = 16, nNodes = 1024, nIters = 1;
         Options options =  new Options();
         CommandLineParser parser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
@@ -88,10 +88,11 @@ public class Coordinator {
         Option bitsOpt = new Option("b", "bits", true, "Number of bits (default: 16)");
         Option SIFOpt = new Option("s", "sif", true, "Export graph to SIF file");
         Option DOTOpt = new Option("d", "dot", true, "Export graph to DOT file");
-        Option logOpt = new Option("l", "log", true, "Append log statistics to JSON file");
+        Option logOpt = new Option("o", "out", true,
+                "Store log statistics to JSON file. If it exists, append the results (default: \"./log.json\")");
         Option helpOpt = new Option("h", "help", false, "Show this help text and exit");
-        Option itOpt = new Option("i", "iterations", true,
-                "If set, perform a fixed number of lookup (random) simulations instead of one for each node");
+        Option itOpt = new Option("l", "lookups", true,
+                "Number of lookup tests per node (default: 1)");
 
         options.addOption(nodesOpt).addOption(bitsOpt).addOption(SIFOpt).addOption(DOTOpt)
                 .addOption(logOpt).addOption(helpOpt).addOption(itOpt);
@@ -110,10 +111,8 @@ public class Coordinator {
             if (cmd.getOptionValue("bits") != null)
                 nBits = Integer.parseInt(cmd.getOptionValue("bits"));
 
-            if (cmd.getOptionValue("iterations") != null)
-                nIters = Integer.parseInt(cmd.getOptionValue("iterations"));
-            else
-                nIters = nNodes;
+            if (cmd.getOptionValue("lookups") != null)
+                nIters = Integer.parseInt(cmd.getOptionValue("lookups"));
         } catch (NumberFormatException e) {
             System.err.println("Arguments must be integers.");
             System.exit(1);
@@ -134,8 +133,25 @@ public class Coordinator {
         }
 
         JSONObject log = new JSONObject();
-        ArrayList<Node> network = new ArrayList<>(buildNetwork(nBits, nNodes));
         String logPath = "log.json";
+
+        if (cmd.getOptionValue("out") != null) {
+            logPath = cmd.getOptionValue("out");
+
+            try(Reader reader = new BufferedReader(new InputStreamReader(
+                    new FileInputStream(cmd.getOptionValue("out")), "utf-8"))) {
+                log = new JSONObject(new JSONTokener(reader));
+            } catch (FileNotFoundException ignore) {} catch (IOException e) {
+                System.err.println("I/O Exception: " + e.getMessage());
+                System.exit(1);
+            }
+        }
+
+        /* **************************************** GENERATE NETWORK ************************************************ */
+
+        ArrayList<Node> network = new ArrayList<>(buildNetwork(nBits, nNodes));
+        File logFile = new File(logPath);
+        logFile.getParentFile().mkdirs();
 
         if (cmd.getOptionValue("sif") != null)
             writeSIFFile(cmd.getOptionValue("sif"), network);
@@ -143,56 +159,54 @@ public class Coordinator {
         if (cmd.getOptionValue("dot") != null)
             writeDOTFile(cmd.getOptionValue("dot"), network);
 
-        if (cmd.getOptionValue("log") != null) {
-            logPath = cmd.getOptionValue("log");
-
-            try(Reader reader = new BufferedReader(new InputStreamReader(
-                    new FileInputStream(cmd.getOptionValue("log")), "utf-8"))) {
-                log = new JSONObject(new JSONTokener(reader));
-            } catch (FileNotFoundException ignore) {} catch (IOException e) {
-                System.err.println("I/O Exception: " + e.getMessage());
-            }
-        }
-
         /* **************************************** START SIMULATION ************************************************ */
 
-        System.out.print("\nRunning simulations: 0 of " + nIters + ".\r");
+        System.out.print("\nRunning simulations: 0 of " + nIters*nNodes + ".\r");
         Random random = new Random();
         byte[] bytes = new byte[32];
-        ArrayList<Integer> keys = new ArrayList<>();
-        ArrayList<Integer> pathLength = new ArrayList<>();
+        HashMap<Integer, Integer> gapHist = new HashMap<>();
+        HashMap<Integer, Integer> pathLengthHist = new HashMap<>();
         HashMap<Node, Integer> queries = new HashMap<>();
-
-        for (int i = 0; i < nIters; i++) {
-            Node node = network.get(cmd.getOptionValue("iterations") != null? random.nextInt(nNodes) : i);
-            random.nextBytes(bytes);
-            Identifier id = new Identifier(bytes);
-            List<Node> path = node.getPathTo(id);
-            pathLength.add(path.size() - 1);
-            path.forEach(n -> queries.compute(n, (k, v) -> { if (v == null) return 1; else return v + 1; }));
-            System.out.print("Running simulations: " + (i + 1) + " of " + nIters + ".\r");
-        }
-
-        System.out.print("\nWriting statistics... ");
+        HashMap<Node, Integer> endNodes = new HashMap<>();
+        int iters = 0;
 
         for (Node node: network) {
             Identifier gap = node.getId().subtract(node.getPredecessor().getId());
-            keys.add(gap.getValue());
+            gapHist.compute(gap.getValue(), (k, v) -> v == null ? 1 : v + 1);
             queries.putIfAbsent(node, 0);
+
+            for (int i = 0; i < nIters; i++) {
+                random.nextBytes(bytes);
+                Identifier id = new Identifier(bytes);
+                List<Node> path = node.getPathTo(id);
+                pathLengthHist.compute(path.size() - 1, (k, v) -> v == null ? 1 : v + 1);
+                path.forEach(n -> queries.compute(n, (k, v) -> v == null ? 1 : v + 1));
+                endNodes.compute(path.get(path.size() - 1), (k, v) -> v == null ? 1 : v + 1);
+                System.out.print("Running simulations: " + ++iters + " of " + nIters*nNodes + ".\r");
+            }
         }
 
+        /* **************************************** WRITE STATISTICS ************************************************ */
+
+        System.out.print("\nWriting statistics... ");
         HashMap<String, Object> results = new HashMap<>();
+        HashMap<Integer, Integer> queryHist = new HashMap<>();
+        HashMap<Integer, Integer> endNodeHist = new HashMap<>();
+        queries.values().forEach(q -> queryHist.compute(q, (k, v) -> v == null ? 1 : v + 1));
+        endNodes.values().forEach(n -> endNodeHist.compute(n, (k, v) -> v == null ? 1 : v + 1));
+
         results.put("bits", nBits);
         results.put("nodes", nNodes);
         results.put("iterations", nIters);
-        results.put("gaps", keys);
-        results.put("pathLengths", pathLength);
-        results.put("queries", network.stream().map(queries::get).collect(Collectors.toList()));
+        results.put("gaps", gapHist);
+        results.put("pathLengths", pathLengthHist);
+        results.put("queries", queryHist);
+        results.put("endNodes", endNodeHist);
 
         log.append("experiments", new JSONObject(results));
 
         try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream(logPath), "utf-8"))) {
+                new FileOutputStream(logFile), "utf-8"))) {
             log.write(writer, 3,0);
         } catch (IOException e) {
             System.err.println("I/O Exception: " + e.getMessage());
