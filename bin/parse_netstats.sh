f649af12e110397f90303a6bb89bf6ae21199a48
Francesco Landolfi <fran.landolfi@gmail.com>
2018-04-20 20:29:18 +0200

    Fix parse ordering

--- bin/parse_netstats.sh
+++ bin/parse_netstats.sh
@@ -6,10 +6,11 @@ OUT_DIR="./summary"
 OUT_FILE="${OUT_DIR}/stats.csv"
 
 mkdir -p ${OUT_DIR}
-echo -e "nodes\tcc\tdiameter\tradius" > ${OUT_FILE}
+echo -e "cc\tdiameter\tradius\tnodes" > ${OUT_FILE}
 
-grep -hwr ./netstats/multi*.netstats -e "nodeCount Integer" -e "cc Double" \
+grep -hwr ./netstats/multi_* --exclude=./netstats/multi_2.netstats \
+        -e "nodeCount Integer" -e "cc Double" \
         -e "diameter Integer" -e "radius Integer" \
     | grep -Eo "[0-9.]+" \
     | tr '\n' '\t' \
-    | grep -Po "[0-9]+\t[0-9]+\.[0-9]+\t[0-9]+\t[0-9]+" >> ${OUT_FILE}
+    | grep -Po "[0-9]+\.[0-9]+\t[0-9]+\t[0-9]+\t[0-9]+" >> ${OUT_FILE}
